c31786772dad42d25476ea2836c1710a3c2f5597
iNeros <38114920+iNeros@users.noreply.github.com>
2021-05-03 21:54:57 -0500

    Initial Commit
    Project Creation

---
+++ src/components/footer-home/footer.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="footerHome">
+    <v-footer
+        color="#5D576B"
+        padless
+        height="150px"
+      >
+        <v-row
+          justify="center"
+          no-gutters
+        >
+          <v-btn
+            v-for="link in links"
+            :key="link"
+            color="white"
+            text
+            rounded
+            class="my-2"
+          >
+            {{ link }}
+          </v-btn>
+          <v-col
+            class="py-4 text-center white--text"
+            cols="12"
+          >
+            {{ new Date().getFullYear() }} — <strong>XicoClass</strong>
+          </v-col>
+        </v-row>
+      </v-footer>
+  </div>
+</template>
+
+<script>
+// import  component  from '@/components/foler-name/component.vue'
+
+export default {
+  name: "footerHome",
+  component: {},
+  data: () => ({
+    links: [
+        'Inicio',
+        'Apartado Legal',
+        'Equipo',
+        'Acerca De'
+
+      ],
+  }),
+};
+</script>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap");
+
+.text-redes {
+  width: 250px;
+}
+.subtitle-text {
+  font-family: "Montserrat";
+  color: white !important;
+}
+</style>
---
+++ src/components/navbar-home/navbar.vue
@@ -0,0 +1,171 @@
+<template>
+  <div class="navbar">
+    <v-app-bar
+      color="#5D576B"
+      height="100px"
+      dense
+      fixed
+      dark
+      class="navbar-generals"
+    >
+      <!--#region              LOGO -- IZQUIERDA    -->
+      <div class="img-logo mx-5">
+        <img
+          class="mx-10"
+          src="@/assets/media/logos/letras.png"
+          alt="XICO CLASS"
+          width="200"
+          height="80"
+        />
+      </div>
+      <v-spacer></v-spacer>
+
+      <!--#endregion-->
+
+      <!--#region            VISTA DEL MENU DE SECCIONES EN WIDE VIEW                          -->
+      <div class="hidden-sm-and-down mx-12">
+        <v-row justify="space-around">
+          <v-menu offset-y dark>
+            <template v-slot:activator="{ on, attrs }">
+              <div class="menu-text">
+                <v-btn href="#" depressed plain class="menu-text btn-selected">
+                  Inicio
+                </v-btn>
+                <v-btn href="#" depressed plain class="menu-text">
+                  Avisos
+                </v-btn>
+                <v-btn href="#" depressed plain class="menu-text">
+                  Biblioteca
+                </v-btn>
+                <v-btn href="#" depressed plain class="menu-text">
+                  Mi Portal
+                </v-btn>
+                <v-btn
+                  depressed
+                  plain
+                  v-bind="attrs"
+                  v-on="on"
+                  class="menu-text"
+                >
+                  Mi Perfil
+                </v-btn>
+              </div>
+            </template>
+            <v-list light>
+              <v-list-item>
+                <v-list-item-title
+                  ><v-btn href="#" depressed plain
+                    >Mi Perfil</v-btn
+                  ></v-list-item-title
+                >
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-title
+                  ><v-btn href="" depressed plain
+                    >Cerrar Sesion</v-btn
+                  ></v-list-item-title
+                >
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-row>
+      </div>
+      <!--#endregion -->
+
+      <!--#region            VISTA DEL MENU DE SECCIONES EN DISPOSITIVOS MOVILES               -->
+
+      <div class="hidden-md-and-up">
+        <v-menu>
+          <template v-slot:activator="{ on, att }">
+            <v-btn dark plain v-bind="att" v-on="on" rounded>
+              <v-icon color="white">mdi-dots-vertical</v-icon>
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item>
+              <v-list-item-title
+                ><v-btn
+                  id="navbar-inicio"
+                  class="btn-selected"
+                  href="/#"
+                  depressed
+                  plain
+                  >Inicio</v-btn
+                ></v-list-item-title
+              >
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title
+                ><v-btn id="navbar-avisos" href="/#" depressed plain
+                  >Avisos</v-btn
+                ></v-list-item-title
+              >
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title
+                ><v-btn id="navbar-biblio" href="/#" depressed plain
+                  >Biblioteca</v-btn
+                ></v-list-item-title
+              >
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title
+                ><v-btn id="navbar-portal" href="/" depressed plain
+                  >Mi Portal</v-btn
+                ></v-list-item-title
+              >
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title
+                ><v-btn id="navbar-perfil" href="/" depressed plain
+                  >Mi Perfil</v-btn
+                ></v-list-item-title
+              >
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-title
+                ><v-btn color="red" href="/" depressed plain
+                  >Cerrar Sesion</v-btn
+                ></v-list-item-title
+              >
+            </v-list-item>
+          </v-list>
+        </v-menu>
+      </div>
+
+      <!--#endregion -->
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+// import  component  from '@/components/foler-name/component.vue'
+
+export default {
+  name: "navbar",
+
+  components: {},
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+.navbar {
+  display: block;
+  height: 100px;
+  width: 100%;
+}
+</style>
+
+<style scoped>
+.menu-text {
+  color: white !important;
+  font-size: 18px !important;
+  font-family: "Montserrat", sans-serif;
+}
+.btn-selected {
+  color: #30dba0 !important;
+}
+</style>
