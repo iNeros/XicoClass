1a4a7269534778efca6ea7fdf0bdc729c4bda0ce
michelchable <michel_yafte@hotmail.com>
2021-05-15 13:25:24 -0500

    Biblioteca + Redirigir NavBar

--- src/views/Biblioteca.vue
+++ src/views/Biblioteca.vue
@@ -1,13 +1,33 @@
 <template>
-  <div class="Biblioteca">
-    <h3>HOLA DESDE LA BIBLIIOTECASSADD</h3>
+  <div class="home">
+    <navbar></navbar>
+    <h1>Es que te quieroooo neroooo</h1>
+    <div class="spacer"></div>
+    <footerHome></footerHome>
   </div>
 </template>
 
 <script>
+// import  component  from '@/components/foler-name/component.vue'
+import navbar from "@/components/navbar-home/navbar.vue";
+import footerHome from "@/components/footer-home/footer.vue";
+
 export default {
   name: "Biblioteca",
+
+  components: {
+    navbar,
+    footerHome
+  },
+  data() {
+    return {};
+  },
 };
 </script>
 
-<style scoped></style>
+<style>
+.spacer {
+  display: block;
+  height: 20px;
+}
+</style>
