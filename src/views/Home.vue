da77c35c9fa6f93e0939a9127d1d67fcf8c4b061
iNeros <38114920+iNeros@users.noreply.github.com>
2021-05-14 21:10:26 -0500

    Lint Changes
    Cambios añadidos de linter , para la integracion correcta del codigo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,42 +4,10 @@
     <carouselHome> </carouselHome>
     <!--AGREGAR AQUI DISEÑO PARALAX PARA CUANDO HAGA SCROLL HACIA LAS IMAGENES 
     SE BAJE A UN AXIS ESPECIFICO EN Y ; (QUE SE VEAN LAS 2 IMAGENES POR COMPLETO)  -->
-    
-    <!--#region ImagenesHome -->
-    <div class="center-info"> 
-        <v-row align-content="space-around" class="my-0 py-0">
-          <v-col cols="12" sm="12" align-self="center" class="my-0 py-0">
-            <v-card
-            flat
-            >
-              <v-img 
-              class="img-borders"
-              src="@/assets/media/holders/1600x500.png"
-              max-height="500px"
-              max-width="80%"
-              ></v-img>
-            </v-card>
-          </v-col>
-        </v-row>
-        <v-row align-content="space-around" class="my-0 py-0">
-          <v-col cols="12" sm="12" align-self="center" class="my-0 py-0">
-            <v-card
-            flat
-            >
-              <v-img 
-              class="img-borders"
-              src="@/assets/media/holders/1600x500.png"
-              max-height="500px"
-              max-width="80%"
-              ></v-img>
-            </v-card>
-          </v-col>
-        </v-row>
-    </div>
-    <!--#endregion-->
-    
+    <centerImages> </centerImages>
+
     <div class="spacer"></div>
-  <footerHome></footerHome>
+    <footerHome></footerHome>
   </div>
 </template>
 
@@ -47,7 +15,8 @@
 // import  component  from '@/components/foler-name/component.vue'
 import navbar from "@/components/navbar-home/navbar.vue";
 import footerHome from "@/components/footer-home/footer.vue";
-import carouselHome from "@/components/home/carousel.vue"
+import carouselHome from "@/components/home/carousel.vue";
+import centerImages from "@/components/home/centerImages.vue";
 
 export default {
   name: "Home",
@@ -55,7 +24,8 @@ export default {
   components: {
     navbar,
     footerHome,
-    carouselHome
+    carouselHome,
+    centerImages,
   },
   data() {
     return {};
@@ -64,17 +34,8 @@ export default {
 </script>
 
 <style>
-.center-info{
-  margin-top: 10px;
-  display: block;
-}
-.spacer{
+.spacer {
   display: block;
   height: 20px;
 }
-.img-borders{
-  margin-right: auto!important;
-  margin-left: auto!important;
-}
-
 </style>
