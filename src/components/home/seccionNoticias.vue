ccd63ae5c254f13bdbef048824e3734cf0e0ac09
vecc-code <othonviveros06@gmail.com>
2021-05-18 16:44:13 -0500

    Update seccionNoticias.vue

--- src/components/home/seccionNoticias.vue
+++ src/components/home/seccionNoticias.vue
@@ -1,20 +1,94 @@
 <template>
-
-<div>
-  <h1>Hola</h1>
-</div>
-<!--
   <v-container fluid>
     <v-row class="">
-      <v-col cols="8" class="">
-        <h1>hola</h1>
-        
-      </v-col>
-      <v-col cols="4" class="">
 
+      <v-col
+      cols="12" xs="6" lg="8" class="">
+      <h2
+      style="color: #f45b69"
+      id="ods"
+      >Avisos recientes
+      </h2>
+      <hr class="mb-3" color="#f45b69" id="od" />
+      <v-card
+      color="#385F73"
+      dark
+      >
+      <v-card-title class="text-h5">
+        Aviso 1
+      </v-card-title>
+        <v-card-subtitle>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet maiores quo atque? Accusamus adipisci cumque aut nobis sit ullam at beatae quidem deleniti sunt corporis, ducimus temporibus recusandae, voluptatem voluptatum.
+        </v-card-subtitle>
+      <v-card-actions>
+        <v-btn text>
+          Leer mas
+        </v-btn>
+      </v-card-actions>
+      </v-card>
+      
+      </v-col>
+      
+      
+      <v-col cols="12" xs="6" lg="4" class="">
+        <h2
+        class="mx-auto ml-6"
+        style="color: #1565C0"
+        >Redes Sociales
+        </h2>
+        <v-card
+        class="mx-auto"
+        color="#26c6da"
+        dark
+        max-width="400"
+        >
+        <v-card-title>
+          <v-icon
+            large
+            left
+          >
+            mdi-twitter
+          </v-icon>
+          <span class="title font-weight-light">Twitter</span>
+        </v-card-title>
+
+        <v-card-text class="headline font-weight-bold">
+          "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well."
+        </v-card-text>
+
+        <v-card-actions>
+          <v-list-item class="grow">
+            <v-list-item-avatar color="grey darken-3">
+              <v-img
+                class="elevation-6"
+                alt=""
+                src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+              ></v-img>
+            </v-list-item-avatar>
+
+            <v-list-item-content>
+              <v-list-item-title>Evan You</v-list-item-title>
+            </v-list-item-content>
+
+            <v-row
+              align="center"
+              justify="end"
+            >
+              <v-icon class="mr-1">
+                mdi-heart
+              </v-icon>
+              <span class="subheading mr-2">256</span>
+              <span class="mr-1">·</span>
+              <v-icon class="mr-1">
+                mdi-share-variant
+              </v-icon>
+              <span class="subheading">45</span>
+            </v-row>
+          </v-list-item>
+        </v-card-actions>
+      </v-card>
       </v-col>
     </v-row>
-  </v-container> -->
+  </v-container>
 </template>
 
 <script>
