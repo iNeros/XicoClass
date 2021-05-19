d5a05165e3f55959a9aaf5f7547fb8387f4223d4
vecc-code <othonviveros06@gmail.com>
2021-05-18 22:19:29 -0500

    Linter a centerImage

--- src/components/home/centerImages.vue
+++ src/components/home/centerImages.vue
@@ -7,29 +7,43 @@
     </v-row>
     -->
     <!-- Con Propiedad de HOVER -->
-    <v-row >
-      <v-col cols="6" xs="12" sm="12" md="6" lg="6" xl="6" class="my-0 py-0 mx-0 px-0">
+    <v-row>
+      <v-col
+        cols="6"
+        xs="12"
+        sm="12"
+        md="6"
+        lg="6"
+        xl="6"
+        class="my-0 py-0 mx-0 px-0"
+      >
         <v-hover v-slot="{ hover }" open-delay="100">
           <v-card :elevation="hover ? 16 : 2" :class="{ 'on-hover': hover }">
             <a href="/Avisos">
-            <v-img
-            src="@/assets/media/holders/imgCentroMob1.png"></v-img>
+              <v-img src="@/assets/media/holders/imgCentroMob1.png"></v-img>
             </a>
           </v-card>
         </v-hover>
       </v-col>
 
-      <v-col cols="6" xs="12" sm="12" md="6" lg="6" xl="6" class="my-0 py-0 mx-0 px-0">
+      <v-col
+        cols="6"
+        xs="12"
+        sm="12"
+        md="6"
+        lg="6"
+        xl="6"
+        class="my-0 py-0 mx-0 px-0"
+      >
         <v-hover v-slot="{ hover }" open-delay="100">
           <v-card :elevation="hover ? 16 : 2" :class="{ 'on-hover': hover }">
             <a href="/Biblioteca">
-            <v-img src="@/assets/media/holders/imgCentroMob2.png"></v-img>
+              <v-img src="@/assets/media/holders/imgCentroMob2.png"></v-img>
             </a>
           </v-card>
         </v-hover>
       </v-col>
     </v-row>
-
   </v-container>
 </template>
 
@@ -68,7 +82,7 @@ export default {
   margin-right: auto !important;
   margin-left: auto !important;
 }
-.full-image{
+.full-image {
   width: 100%;
   height: 100%;
 }
