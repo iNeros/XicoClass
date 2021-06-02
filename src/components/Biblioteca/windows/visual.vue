64741e079dc2b4caf64e5648cac3234934a5a4ca
michelchable <michel_yafte@hotmail.com>
2021-06-01 20:50:58 -0500

    Update visual.vue

--- src/components/Biblioteca/windows/visual.vue
+++ src/components/Biblioteca/windows/visual.vue
@@ -1,7 +1,31 @@
 <template>
-  <div class="visual">
-    <h1>hello from visual</h1>
-  </div>
+  <v-card
+    class="mx-auto"
+    max-width="300"
+    tile
+  >
+    <v-list dense
+    class = "menu-text">
+      <v-subheader>REPORTS</v-subheader>
+      <v-list-item-group
+        v-model="selectedItem"
+        color="#30dba0"
+      >
+        <v-list-item>Text1</v-list-item>
+        <v-list-item>Text2</v-list-item>
+        <v-list-item>Text3</v-list-item>
+        <v-list-item>Text1</v-list-item>
+        <v-list-item>Text2</v-list-item>
+        <v-list-item>Text3</v-list-item>
+        <v-list-item>Text1</v-list-item>
+        <v-list-item>Text2</v-list-item>
+        <v-list-item>Text3</v-list-item>
+        <v-list-item>Text1</v-list-item>
+        <v-list-item>Text2</v-list-item>
+        <v-list-item>Text3</v-list-item>
+      </v-list-item-group>
+    </v-list>
+  </v-card>
 </template>
 
 <script>
@@ -13,4 +37,11 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap");
+.menu-text {
+  color: #5D576B !important;
+  font-size: 18px !important;
+  font-family: "Montserrat", sans-serif;
+}
+</style>
