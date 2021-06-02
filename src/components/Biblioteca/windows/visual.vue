d1079d5296ef98925100fcf3a409a1e8ffb97b9d
michelchable <michel_yafte@hotmail.com>
2021-06-01 23:39:33 -0500

    Update visual.vue

--- src/components/Biblioteca/windows/visual.vue
+++ src/components/Biblioteca/windows/visual.vue
@@ -1,16 +1,25 @@
 <template>
-  <v-card
-    class="mx-auto"
-    max-width="300"
-    tile
-  >
-    <v-list dense
-    class = "menu-text">
+  
+    <v-list dense>
       <v-subheader>REPORTS</v-subheader>
       <v-list-item-group
         v-model="selectedItem"
         color="#30dba0"
       >
+        <v-divider></v-divider>
+        <v-list-item class = "menu-tex" v-for="n in numeros" :key="n" v-text="n"></v-list-item>
+        <!--
+        <v-list-item>Text2</v-list-item>
+        <v-list-item>Text3</v-list-item>
+        <v-list-item>Text1</v-list-item>
+        <v-list-item>Text2</v-list-item>
+        <v-list-item>Text3</v-list-item>
+        <v-list-item>Text1</v-list-item>
+        <v-list-item>Text2</v-list-item>
+        <v-list-item>Text3</v-list-item>
+        <v-list-item>Text1</v-list-item>
+        <v-list-item>Text2</v-list-item>
+        <v-list-item>Text3</v-list-item>
         <v-list-item>Text1</v-list-item>
         <v-list-item>Text2</v-list-item>
         <v-list-item>Text3</v-list-item>
@@ -23,16 +32,19 @@
         <v-list-item>Text1</v-list-item>
         <v-list-item>Text2</v-list-item>
         <v-list-item>Text3</v-list-item>
+        -->
       </v-list-item-group>
     </v-list>
-  </v-card>
+    
 </template>
 
 <script>
 export default {
   name: "visual",
   data() {
-    return {};
+    return {
+      numeros:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18],
+    };
   },
 };
 </script>
@@ -43,5 +55,11 @@ export default {
   color: #5D576B !important;
   font-size: 18px !important;
   font-family: "Montserrat", sans-serif;
+}
+.v-list{
+  height:60vh;/* or any height you want */
+  width:300px;
+  overflow-y:auto;
+  margin-left: 0px;
 }
 </style>
