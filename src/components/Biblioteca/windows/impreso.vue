e253ddddcf87a6447ef7c7567e3a17e6f49d831e
michelchable <michel_yafte@hotmail.com>
2021-06-07 23:11:20 -0500

    AVANCE IMPRESO

--- src/components/Biblioteca/windows/impreso.vue
+++ src/components/Biblioteca/windows/impreso.vue
@@ -1,3 +1,4 @@
+<!--
 <template
 v-for="n in datos"
 :key= "n">
@@ -26,6 +27,46 @@ v-for="n in datos"
      
     </v-btn>
   </div>
+</template>
+-->
+
+<template>
+  <v-container fluid>
+    <v-row class="fill-height mt-6 mx-6"> 
+      <template v-for="n in datos">
+        <v-col :key= "n" cols="12" xl="1" lg="2" md="4" sm="6" xs="12">
+          <v-hover v-slot="{ hover }" open-delay="200">
+            <v-card 
+              :elevation="hover ? 12 : 2"
+              :class="{ 'on-hover': hover }"
+              shaped              
+              color="#5AA9E6"
+              class="card"
+              @click="descargarArchivo(n.ruta)"
+            >
+              <v-card-title>
+                <v-row>
+                  <v-col cols="12" lg="12" md="12" xs="12">
+                      <v-img v-if ="n.tipo == 1" src="@/assets/media/logos/icon-pdf.png" class="imagen mx-6">
+                      </v-img>
+                      <v-img v-if ="n.tipo == 2" src="@/assets/media/logos/icon-doc.png" class="imagen mx-4">
+                      </v-img>
+                      <v-img v-if ="n.tipo == 3" :src=n.ruta class="imagen mx-4">
+                      </v-img>
+                      <v-img v-if ="n.tipo > 8" src="@/assets/media/logos/icon-default.png" class="imagen mx-4">
+                      </v-img>
+                  </v-col>
+                </v-row>
+              </v-card-title>
+                <p class="subtitulo text-center">
+                  {{ n.titulo }}
+                </p>
+            </v-card>
+          </v-hover>
+        </v-col>
+      </template>
+      </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -63,5 +104,22 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap');
+.card{
+  min-height: 150px;
+  max-height: 150px;
+}
+.imagen{
+  display: block;
+  width: 60px;
+  height: 60px;
+}
+.subtitulo {
+  font-family: "Poppins";
+  font-weight: 500px;
+  font-size: 14px;
+  color: white;
+}
+</style>
 
