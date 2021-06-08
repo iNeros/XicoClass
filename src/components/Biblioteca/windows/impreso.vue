748d2ecae4de86254fb4dabeb6b86f0350729b3d
michelchable <michel_yafte@hotmail.com>
2021-06-07 21:38:19 -0500

    Update impreso.vue

--- src/components/Biblioteca/windows/impreso.vue
+++ src/components/Biblioteca/windows/impreso.vue
@@ -1,39 +1,67 @@
-<template>
+<template
+v-for="n in datos"
+:key= "n">
   <div>
-    <v-btn
-      v-for="n in links"
-      :key= "n"
-      class="ma-2" 
-      outlined 
-      @click="descargarArchivo(n)"
-      color="blue lighten-2"
-    >
-      Archivo
-      <v-icon 
-        dark
+    <v-icon v-if="n.tipo == 1"
         right
+      >
+        mdi-download-circle
+      </v-icon>
+      <v-icon v-else
       >
         mdi-cloud-upload
       </v-icon>
     
+    <v-btn
+      
+      
+      class="ma-2" 
+      outlined 
+      rounded
+      @click="descargarArchivo(n.ruta)"
+      color="blue lighten-2"
+    >
+    {{n.tipo}}
+      
+     
     </v-btn>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "impreso",
   data() {
     return {
       links:[],
+      datos:[],
     };
   },
   methods:{
     descargarArchivo(id) {
       window.open("" + id, "_blank");
     },
+    obtenerImpreso() {
+      axios
+        .get(
+          "https://xicoclassapi.azurewebsites.net/impreso.php"
+        )
+        .then((r) => {
+          this.datos = r.data;
+          console.log(this.datos);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+  },
+  mounted() {
+    this.obtenerImpreso();
   },
 };
 </script>
 
 <style scoped></style>
+
