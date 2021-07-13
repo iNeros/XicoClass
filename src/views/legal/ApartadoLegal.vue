b59432f8ae40d8f633a6da05ed9f7e80af0918c8
vecc-code <othonviveros06@gmail.com>
2021-07-12 20:25:10 -0500

    apartado legal DONE

--- src/views/legal/ApartadoLegal.vue
+++ src/views/legal/ApartadoLegal.vue
@@ -2,8 +2,9 @@
   <div class="ApartadoLegal">
     <navbar></navbar>
     <v-container>
-      <h1 class="titulo mt-7">Apartado Legal</h1>
-      <h1>Aviso legal y términos de uso</h1>
+      <h1 class="titulo mt-8">Apartado Legal XicoClass</h1>
+      <h1 class="mt-8">Aviso legal y términos de uso</h1>
+      <br />
       <p>
         En este espacio, el USUARIO, podrá encontrar toda la información
         relativa a los términos y condiciones legales que definen las relaciones
@@ -18,24 +19,32 @@
         julio, de servicios de la sociedad de la información y de comercio
         electrónico.
         <br />
-        CONDICIONES GENERALES DE USO Las presentes Condiciones Generales regulan
-        el uso (incluyendo el mero acceso) de las páginas de la web, integrantes
-        del sitio web de https://xicoclass.firebaseapp.com/ incluidos los
-        contenidos y servicios puestos a disposición en ellas. Toda persona que
-        acceda a la web, https://xicoclass.firebaseapp.com/ (“Usuario”) acepta
-        someterse a las Condiciones Generales vigentes en cada momento del
-        portal https://xicoclass.firebaseapp.com/. DATOS PERSONALES QUE
-        RECABAMOS Y CÓMO LO HACEMOS Leer Política de Privacidad COMPROMISOS Y
-        OBLIGACIONES DE LOS USUARIOS El Usuario queda informado, y acepta, que
-        el acceso a la presente web no supone, en modo alguno, el inicio de una
-        relación comercial con https://xicoclass.firebaseapp.com/. De esta
-        forma, el usuario se compromete a utilizar el sitio Web, sus servicios y
-        contenidos sin contravenir la legislación vigente, la buena fe y el
-        orden público. Queda prohibido el uso de la web, con fines ilícitos o
-        lesivos, o que, de cualquier forma, puedan causar perjuicio o impedir el
-        normal funcionamiento del sitio web. Respecto de los contenidos de esta
-        web, se prohíbe:Su reproducción, distribución o modificación, total o
-        parcial, a menos que se cuente con la autorización de sus legítimos
+        <br />
+      </p>
+      <h1>CONDICIONES GENERALES DE USO</h1>
+      <br />
+      <p>
+        Las presentes Condiciones Generales regulan el uso (incluyendo el mero
+        acceso) de las páginas de la web, integrantes del sitio web de
+        https://xicoclass.firebaseapp.com/ incluidos los contenidos y servicios
+        puestos a disposición en ellas. Toda persona que acceda a la web,
+        https://xicoclass.firebaseapp.com/ (“Usuario”) acepta someterse a las
+        Condiciones Generales vigentes en cada momento del portal
+        https://xicoclass.firebaseapp.com/.
+      </p>
+      <h1>COMPROMISOS Y OBLIGACIONES DE LOS USUARIOS</h1>
+      <br />
+      <p>
+        El Usuario queda informado, y acepta, que el acceso a la presente web no
+        supone, en modo alguno, el inicio de una relación comercial con
+        https://xicoclass.firebaseapp.com/. De esta forma, el usuario se
+        compromete a utilizar el sitio Web, sus servicios y contenidos sin
+        contravenir la legislación vigente, la buena fe y el orden público.
+        Queda prohibido el uso de la web, con fines ilícitos o lesivos, o que,
+        de cualquier forma, puedan causar perjuicio o impedir el normal
+        funcionamiento del sitio web. Respecto de los contenidos de esta web, se
+        prohíbe:Su reproducción, distribución o modificación, total o parcial, a
+        menos que se cuente con la autorización de sus legítimos
         titulares;Cualquier vulneración de los derechos del prestador o de los
         legítimos titulares;Su utilización para fines comerciales o
         publicitarios. En la utilización de la web,
@@ -49,8 +58,13 @@
         que, por tanto https://xicoclass.firebaseapp.com/ no puede garantizar la
         inexistencia de virus u otros elementos que puedan producir alteraciones
         en los sistemas informáticos (software y hardware) del Usuario o en sus
-        documentos electrónicos y ficheros contenidos en los mismos. MEDIDAS DE
-        SEGURIDAD Los datos personales comunicados por el usuario a
+        documentos electrónicos y ficheros contenidos en los mismos.
+      </p>
+
+      <h1>MEDIDAS DE SEGURIDAD</h1>
+      <br />
+      <p>
+        Los datos personales comunicados por el usuario a
         https://xicoclass.firebaseapp.com/ pueden ser almacenados en bases de
         datos automatizadas o no, cuya titularidad corresponde en exclusiva a
         https://xicoclass.firebaseapp.com/, asumiendo ésta todas las medidas de
@@ -61,7 +75,11 @@
         https://xicoclass.firebaseapp.com/ utiliza un canal seguro, y los datos
         transmitidos son cifrados gracias a protocolos a https, por tanto,
         garantizamos las mejores condiciones de seguridad para que la
-        confidencialidad de los usuarios esté garantizada. RECLAMACIONES
+        confidencialidad de los usuarios esté garantizada.
+      </p>
+      <h1>RECLAMACIONES</h1>
+      <br />
+      <p>
         https://xicoclass.firebaseapp.com/ informa que existen hojas de
         reclamación a disposición de usuarios y clientes. El Usuario podrá
         realizar reclamaciones solicitando su hoja de reclamación o remitiendo
@@ -71,92 +89,121 @@
         correo electrónico a: , si lo desea adjuntando el siguiente formulario
         de reclamación: El servicio/producto: Adquirido el día: Nombre del
         usuario: Domicilio del usuario: Firma del usuario (solo si se presenta
-        en papel): Fecha: Motivo de la reclamación: PLATAFORMA DE RESOLUCIÓN DE
-        CONFLICTOS Por si puede ser de tu interés, para someter tus
-        reclamaciones puedes utilizar también la plataforma de resolución de
-        litigios que facilita la Comisión Europea y que se encuentra disponible
-        en el siguiente enlace: http://ec.europa.eu/consumers/odr/ DERECHOS DE
-        PROPIEDAD INTELECTUAL E INDUSTRIAL En virtud de lo dispuesto en los
-        artículos 8 y 32.1, párrafo segundo, de la Ley de Propiedad Intelectual,
-        quedan expresamente prohibidas la reproducción, la distribución y la
-        comunicación pública, incluida su modalidad de puesta a disposición, de
-        la totalidad o parte de los contenidos de esta página web, con fines
-        comerciales, en cualquier soporte y por cualquier medio técnico, sin la
-        autorización de https://xicoclass.firebaseapp.com/. El usuario se
-        compromete a respetar los derechos de Propiedad Intelectual e Industrial
-        titularidad de https://xicoclass.firebaseapp.com/. El usuario conoce y
-        acepta que la totalidad del sitio web, conteniendo sin carácter
-        exhaustivo el texto, software, contenidos (incluyendo estructura,
-        selección, ordenación y presentación de los mismos) podcast,
-        fotografías, material audiovisual y gráficos, está protegida por marcas,
-        derechos de autor y otros derechos legítimos, de acuerdo con los
-        tratados internacionales en los que España es parte y otros derechos de
-        propiedad y leyes de España. En el caso de que un usuario o un tercero
-        consideren que se ha producido una violación de sus legítimos derechos
-        de propiedad intelectual por la introducción de un determinado contenido
-        en la web, deberá notificar dicha circunstancia a
-        https://xicoclass.firebaseapp.com/ indicando: Datos personales del
-        interesado titular de los derechos presuntamente infringidos, o indicar
-        la representación con la que actúa en caso de que la reclamación la
-        presente un tercero distinto del interesado. Señalar los contenidos
-        protegidos por los derechos de propiedad intelectual y su ubicación en
-        la web, la acreditación de los derechos de propiedad intelectual
-        señalados y declaración expresa en la que el interesado se
+        en papel): Fecha: Motivo de la reclamación:
+      </p>
+
+      <h1>PLATAFORMA DE RESOLUCIÓN DE CONFLICTOS</h1>
+      <br />
+      <p>
+        Por si puede ser de tu interés, para someter tus reclamaciones puedes
+        utilizar también la plataforma de resolución de litigios que facilita la
+        Comisión Europea y que se encuentra disponible en el siguiente enlace:
+        http://ec.europa.eu/consumers/odr/
+      </p>
+      <h1>DERECHOS DE PROPIEDAD INTELECTUAL E INDUSTRIAL</h1>
+      <br />
+      <p>
+        En virtud de lo dispuesto en los artículos 8 y 32.1, párrafo segundo, de
+        la Ley de Propiedad Intelectual, quedan expresamente prohibidas la
+        reproducción, la distribución y la comunicación pública, incluida su
+        modalidad de puesta a disposición, de la totalidad o parte de los
+        contenidos de esta página web, con fines comerciales, en cualquier
+        soporte y por cualquier medio técnico, sin la autorización de
+        https://xicoclass.firebaseapp.com/. El usuario se compromete a respetar
+        los derechos de Propiedad Intelectual e Industrial titularidad de
+        https://xicoclass.firebaseapp.com/. El usuario conoce y acepta que la
+        totalidad del sitio web, conteniendo sin carácter exhaustivo el texto,
+        software, contenidos (incluyendo estructura, selección, ordenación y
+        presentación de los mismos) podcast, fotografías, material audiovisual y
+        gráficos, está protegida por marcas, derechos de autor y otros derechos
+        legítimos, de acuerdo con los tratados internacionales en los que España
+        es parte y otros derechos de propiedad y leyes de España. En el caso de
+        que un usuario o un tercero consideren que se ha producido una violación
+        de sus legítimos derechos de propiedad intelectual por la introducción
+        de un determinado contenido en la web, deberá notificar dicha
+        circunstancia a https://xicoclass.firebaseapp.com/ indicando: Datos
+        personales del interesado titular de los derechos presuntamente
+        infringidos, o indicar la representación con la que actúa en caso de que
+        la reclamación la presente un tercero distinto del interesado. Señalar
+        los contenidos protegidos por los derechos de propiedad intelectual y su
+        ubicación en la web, la acreditación de los derechos de propiedad
+        intelectual señalados y declaración expresa en la que el interesado se
         responsabiliza de la veracidad de las informaciones facilitadas en la
-        notificación ENLACES EXTERNOS Las páginas de la web
-        https://xicoclass.firebaseapp.com/, podría proporcionar enlaces a otros
-        sitios web propios y contenidos que son propiedad de terceros. El único
-        objeto de los enlaces es proporcionar al Usuario la posibilidad de
-        acceder a dichos enlaces. https://xicoclass.firebaseapp.com/ no se
-        responsabiliza en ningún caso de los resultados que puedan derivarse al
-        Usuario por acceso a dichos enlaces. Asimismo, el usuario encontrará
-        dentro de este sitio, páginas, promociones, programas de afiliados que
-        acceden a los hábitos de navegación de los usuarios para establecer
-        perfiles. Esta información siempre es anónima y no se identifica al
-        usuario. La Información que se proporcione en estos Sitios patrocinado o
-        enlaces de afiliados está sujeta a las políticas de privacidad que se
-        utilicen en dichos Sitios y no estará sujeta a esta política de
-        privacidad. Por lo que recomendamos ampliamente a los Usuarios a revisar
-        detalladamente las políticas de privacidad de los enlaces de afiliado.
-        El Usuario que se proponga establecer cualquier dispositivo técnico de
-        enlace desde su sitio web al portal https://xicoclass.firebaseapp.com/
-        deberá obtener la autorización previa y escrita de
-        https://xicoclass.firebaseapp.com/ El establecimiento del enlace no
-        implica en ningún caso la existencia de relaciones entre
-        https://xicoclass.firebaseapp.com/ y el propietario del sitio en el que
-        se establezca el enlace, ni la aceptación o aprobación por parte de
-        https://xicoclass.firebaseapp.com/ de sus contenidos o servicios
-        POLÍTICA DE COMENTARIOS En nuestra web y se permiten realizar
-        comentarios para enriquecer los contenidos y realizar consultas. No se
-        admitirán comentarios que no estén relacionados con la temática de esta
-        web, que incluyan difamaciones, agravios, insultos, ataques personales o
-        faltas de respeto en general hacia el autor o hacia otros miembros.
-        También serán suprimidos los comentarios que contengan información que
-        sea obviamente engañosa o falsa, así como los comentarios que contengan
-        información personal, como, por ejemplo, domicilios privado o teléfonos
-        y que vulneren nuestra política de protección de datos. Se desestimará,
+        notificación.
+      </p>
+      <h1>ENLACES EXTERNOS</h1>
+      <br />
+      <p>
+        Las páginas de la web https://xicoclass.firebaseapp.com/, podría
+        proporcionar enlaces a otros sitios web propios y contenidos que son
+        propiedad de terceros. El único objeto de los enlaces es proporcionar al
+        Usuario la posibilidad de acceder a dichos enlaces.
+        https://xicoclass.firebaseapp.com/ no se responsabiliza en ningún caso
+        de los resultados que puedan derivarse al Usuario por acceso a dichos
+        enlaces. Asimismo, el usuario encontrará dentro de este sitio, páginas,
+        promociones, programas de afiliados que acceden a los hábitos de
+        navegación de los usuarios para establecer perfiles. Esta información
+        siempre es anónima y no se identifica al usuario. La Información que se
+        proporcione en estos Sitios patrocinado o enlaces de afiliados está
+        sujeta a las políticas de privacidad que se utilicen en dichos Sitios y
+        no estará sujeta a esta política de privacidad. Por lo que recomendamos
+        ampliamente a los Usuarios a revisar detalladamente las políticas de
+        privacidad de los enlaces de afiliado. El Usuario que se proponga
+        establecer cualquier dispositivo técnico de enlace desde su sitio web al
+        portal https://xicoclass.firebaseapp.com/ deberá obtener la autorización
+        previa y escrita de https://xicoclass.firebaseapp.com/ El
+        establecimiento del enlace no implica en ningún caso la existencia de
+        relaciones entre https://xicoclass.firebaseapp.com/ y el propietario del
+        sitio en el que se establezca el enlace, ni la aceptación o aprobación
+        por parte de https://xicoclass.firebaseapp.com/ de sus contenidos o
+        servicios.
+      </p>
+      <h1>POLÍTICA DE COMENTARIOS</h1>
+      <br />
+      <p>
+        En nuestra web y se permiten realizar comentarios para enriquecer los
+        contenidos y realizar consultas. No se admitirán comentarios que no
+        estén relacionados con la temática de esta web, que incluyan
+        difamaciones, agravios, insultos, ataques personales o faltas de respeto
+        en general hacia el autor o hacia otros miembros. También serán
+        suprimidos los comentarios que contengan información que sea obviamente
+        engañosa o falsa, así como los comentarios que contengan información
+        personal, como, por ejemplo, domicilios privado o teléfonos y que
+        vulneren nuestra política de protección de datos. Se desestimará,
         igualmente, aquellos comentarios creados sólo con fines promocionales de
         una web, persona o colectivo y todo lo que pueda ser considerado spam en
         general. No se permiten comentarios anónimos, así como aquellos
         realizados por una misma persona con distintos apodos. No se
         considerarán tampoco aquellos comentarios que intenten forzar un debate
-        o una toma de postura por otro usuario. EXCLUSIÓN DE GARANTÍAS Y
-        RESPONSABILIDAD El Prestador no otorga ninguna garantía ni se hace
-        responsable, en ningún caso, de los daños y perjuicios de cualquier
-        naturaleza que pudieran traer causa de: La falta de disponibilidad,
-        mantenimiento y efectivo funcionamiento de la web, o de sus servicios y
-        contenidos; La existencia de virus, programas maliciosos o lesivos en
-        los contenidos; El uso ilícito, negligente, fraudulento o contrario a
-        este Aviso Legal; La falta de licitud, calidad, fiabilidad, utilidad y
-        disponibilidad de los servicios prestados por terceros y puestos a
-        disposición de los usuarios en el sitio web. El prestador no se hace
-        responsable bajo ningún concepto de los daños que pudieran dimanar del
-        uso ilegal o indebido de la presente página web. LEY APLICABLE Y
-        JURISDICCIÓN Con carácter general las relaciones entre
+        o una toma de postura por otro usuario.
+      </p>
+      <h1>EXCLUSIÓN DE GARANTÍAS Y RESPONSABILIDAD</h1>
+      <br />
+      <p>
+        El Prestador no otorga ninguna garantía ni se hace responsable, en
+        ningún caso, de los daños y perjuicios de cualquier naturaleza que
+        pudieran traer causa de: La falta de disponibilidad, mantenimiento y
+        efectivo funcionamiento de la web, o de sus servicios y contenidos; La
+        existencia de virus, programas maliciosos o lesivos en los contenidos;
+        El uso ilícito, negligente, fraudulento o contrario a este Aviso Legal;
+        La falta de licitud, calidad, fiabilidad, utilidad y disponibilidad de
+        los servicios prestados por terceros y puestos a disposición de los
+        usuarios en el sitio web. El prestador no se hace responsable bajo
+        ningún concepto de los daños que pudieran dimanar del uso ilegal o
+        indebido de la presente página web.
+      </p>
+      <h1>LEY APLICABLE Y JURISDICCIÓN</h1>
+      <br />
+      <p>
+        Con carácter general las relaciones entre
         https://xicoclass.firebaseapp.com/ con los Usuarios de sus servicios
         telemáticos, presentes en esta web se encuentran sometidas a la
-        legislación y jurisdicción españolas y a los tribunales. CONTACTO En
-        caso de que cualquier Usuario tuviese alguna duda acerca de estas
+        legislación y jurisdicción españolas y a los tribunales.
+      </p>
+      <h1>CONTACTO</h1>
+      <br />
+      <p>
+        En caso de que cualquier Usuario tuviese alguna duda acerca de estas
         Condiciones legales o cualquier comentario sobre el portal
         https://xicoclass.firebaseapp.com/, por favor diríjase a De parte del
         equipo que formamos XicoClass te agradecemos el tiempo dedicado en leer
@@ -191,12 +238,12 @@ export default {
   font-size: 40px;
   line-height: 40px;
   color: #37474f;
-  text-align: left;
+  text-align: center;
 }
 p {
   font-family: "Poppins";
   font-weight: 500px;
   font-size: 16px;
-  text-align: center;
+  text-align: justify;
 }
 </style>
