0fb5dd3baba51ab80c99bad22e53ccbb459f0eb2
vecc-code <othonviveros06@gmail.com>
2021-07-12 19:39:30 -0500

    El equipo DONE

--- src/views/legal/Equipo.vue
+++ src/views/legal/Equipo.vue
@@ -1,16 +1,377 @@
 <template>
   <div class="Equipo">
-    <h3>hello from Equipo</h3>
+    <navbar></navbar>
+    <v-container>
+      <v-row justify="center" justify-lg="center" class="mt-4 mb-2">              
+        <h1 class="titulo-pagina">Miembros del equipo</h1>        
+      </v-row>
+      <v-row  justify="center" >
+        <div class="linea"></div>
+      </v-row>
+      
+      <!-- MAESTRA SALOME-->
+
+      <v-row class="mt-11 mb-4" justify="center">                   
+          <v-card 
+            elevation="3" 
+            width="400" 
+            shaped 
+            outlined
+          >            
+            <div class="d-flex justify-center ">
+              <v-avatar 
+                size="164"
+                class="sombra-avatar"                
+              >
+                <v-img
+                  contain
+                  class="mt-6"
+                  src="@/assets/media/logos/CaraNiña.png"
+                >
+                </v-img>
+              </v-avatar>
+            </div>
+
+            <div class="d-flex justify-center">
+              <v-card-title class="titulo mb-0 pb-0">
+                Mtra. María <br> Salomé Alejandre<br> Apolinar
+              </v-card-title>
+            </div>
+            
+            <v-card-subtitle class="subtitulo pt-1">
+              Lider del proyecto
+            </v-card-subtitle>                           
+            <v-card-text class="texto-centrado fuente-poppins">
+              Lorem ipsum dolor sit amet consectetur 
+              adipisicing elit. Vitae molestias dolorem eum, ab officia 
+              perferendis veniam, consectetur quas obcaecati facilis nisi 
+              fugiat modi, placeat unde quod ducimus optio! Quidem, debitis.
+            </v-card-text>                      
+          </v-card>                 
+      </v-row>
+
+      <!-- NOSOTROS-->
+      <v-row justify-lg="space-around" class="mb-2">
+        <v-col  xl="4" lg="4" md="4" sm="12" xs="12">
+          <v-card 
+            elevation="3" 
+            width="400" 
+            shaped 
+            outlined
+            min-height="450"
+          >            
+            <div class="d-flex justify-center ">
+              <v-avatar 
+                size="164"
+                class="sombra-avatar"                
+              >
+                <v-img
+                  contain
+                  class="mt-6"
+                  src="@/assets/media/logos/CaraNiña.png"
+                >
+                </v-img>
+              </v-avatar>
+            </div>
+
+            <div class="d-flex justify-center">
+              <v-card-title class="titulo mb-0 pb-0">
+                Adriana Abigail <br> Dominguez <br> Morales
+              </v-card-title>
+            </div>
+            
+            <v-card-subtitle class="subtitulo pt-1">
+              Lider del proyecto
+            </v-card-subtitle>                           
+            <v-card-text class="texto-centrado fuente-poppins">
+              Lorem ipsum dolor sit amet consectetur 
+              adipisicing elit. Vitae molestias dolorem eum, ab officia 
+              perferendis veniam, consectetur quas obcaecati facilis nisi 
+              fugiat modi, placeat unde quod ducimus optio! Quidem, debitis.
+            </v-card-text>                      
+          </v-card>          
+        </v-col>
+
+        <v-col  xl="4" lg="4" md="4" sm="12" xs="12">
+          <v-card 
+            elevation="3" 
+            width="400" 
+            shaped 
+            outlined
+            min-height="450"
+          >            
+            <div class="d-flex justify-center ">
+              <v-avatar 
+                size="164"
+                class="sombra-avatar"                
+              >
+                <v-img
+                  contain
+                  class="mt-6 imagen-carta"
+                  src="@/assets/media/logos/CaraNiño.png"
+                >
+                </v-img>
+              </v-avatar>
+            </div>
+
+            <div class="d-flex justify-center">
+              <v-card-title class="titulo mb-0 pb-0">
+                Gibran Camacho Ramoz
+              </v-card-title>
+            </div>
+            
+            <v-card-subtitle class="subtitulo pt-1">
+              Lider del proyecto
+            </v-card-subtitle>                           
+            <v-card-text class="texto-centrado fuente-poppins">
+              Lorem ipsum dolor sit amet consectetur 
+              adipisicing elit. Vitae molestias dolorem eum, ab officia 
+              perferendis veniam, consectetur quas obcaecati facilis nisi 
+              fugiat modi, placeat unde quod ducimus optio! Quidem, debitis.
+            </v-card-text>                      
+          </v-card>          
+        </v-col>
+
+        <v-col  xl="4" lg="4" md="4" sm="12" xs="12">
+          <v-card 
+            elevation="3" 
+            width="400" 
+            shaped 
+            outlined
+            min-height="450"
+          >            
+            <div class="d-flex justify-center ">
+              <v-avatar 
+                size="164"
+                class="sombra-avatar"                
+              >
+                <v-img
+                  contain
+                  class="mt-6 imagen-carta"
+                  src="@/assets/media/logos/CaraNiño.png"
+                >
+                </v-img>
+              </v-avatar>
+            </div>
+
+            <div class="d-flex justify-center">
+              <v-card-title class="titulo mb-0 pb-0">
+                Michel Yafté <br> Chablé Antonio 
+              </v-card-title>
+            </div>
+            
+            <v-card-subtitle class="subtitulo pt-1">
+              Lider del proyecto
+            </v-card-subtitle>                           
+            <v-card-text class="texto-centrado fuente-poppins">
+              Lorem ipsum dolor sit amet consectetur 
+              adipisicing elit. Vitae molestias dolorem eum, ab officia 
+              perferendis veniam, consectetur quas obcaecati facilis nisi 
+              fugiat modi, placeat unde quod ducimus optio! Quidem, debitis.
+            </v-card-text>                      
+          </v-card>          
+        </v-col>
+      </v-row>
+      
+
+      <v-row justify-lg="space-around" class="mb-2">
+        <v-col  xl="4" lg="4" md="4" sm="12" xs="12">
+          <v-card 
+            elevation="3" 
+            width="400" 
+            shaped 
+            outlined
+            min-height="450"
+          >            
+            <div class="d-flex justify-center ">
+              <v-avatar 
+                size="164"
+                class="sombra-avatar"                
+              >
+                <v-img
+                  contain
+                  class="mt-6"
+                  src="@/assets/media/logos/CaraNiño.png"
+                >
+                </v-img>
+              </v-avatar>
+            </div>
+
+            <div class="d-flex justify-center">
+              <v-card-title class="titulo mb-0 pb-0">
+                Raúl Alfonso <br> Tenorio De La O
+              </v-card-title>
+            </div>
+            
+            <v-card-subtitle class="subtitulo pt-1">
+              Lider del proyecto
+            </v-card-subtitle>                           
+            <v-card-text class="texto-centrado fuente-poppins">
+              Lorem ipsum dolor sit amet consectetur 
+              adipisicing elit. Vitae molestias dolorem eum, ab officia 
+              perferendis veniam, consectetur quas obcaecati facilis nisi 
+              fugiat modi, placeat unde quod ducimus optio! Quidem, debitis.
+            </v-card-text>                      
+          </v-card>          
+        </v-col>
+
+        <v-col  xl="4" lg="4" md="4" sm="12" xs="12">
+          <v-card 
+            elevation="3" 
+            width="400" 
+            shaped 
+            outlined
+            min-height="450"
+          >            
+            <div class="d-flex justify-center ">
+              <v-avatar 
+                size="164"
+                class="sombra-avatar"                
+              >
+                <v-img
+                  contain
+                  class="mt-6 imagen-carta"
+                  src="@/assets/media/logos/CaraNiño.png"
+                >
+                </v-img>
+              </v-avatar>
+            </div>
+
+            <div class="d-flex justify-center">
+              <v-card-title class="titulo mb-0 pb-0">
+                Othón Andrés <br> Morales Viveros
+              </v-card-title>
+            </div>
+            
+            <v-card-subtitle class="subtitulo pt-1">
+              Lider del proyecto
+            </v-card-subtitle>                           
+            <v-card-text class="texto-centrado fuente-poppins">
+              Lorem ipsum dolor sit amet consectetur 
+              adipisicing elit. Vitae molestias dolorem eum, ab officia 
+              perferendis veniam, consectetur quas obcaecati facilis nisi 
+              fugiat modi, placeat unde quod ducimus optio! Quidem, debitis.
+            </v-card-text>                      
+          </v-card>          
+        </v-col>
+
+        <v-col  xl="4" lg="4" md="4" sm="12" xs="12">
+          <v-card 
+            elevation="3" 
+            width="400" 
+            shaped 
+            outlined
+            min-height="450"
+          >            
+            <div class="d-flex justify-center ">
+              <v-avatar 
+                size="164"
+                class="sombra-avatar"                
+              >
+                <v-img
+                  contain
+                  class="mt-6 imagen-carta"
+                  src="@/assets/media/logos/CaraNiño.png"
+                >
+                </v-img>
+              </v-avatar>
+            </div>
+
+            <div class="d-flex justify-center">
+              <v-card-title class="titulo mb-0 pb-0">
+                Alejandro Oliva <br> Baizabal
+              </v-card-title>
+            </div>
+            
+            <v-card-subtitle class="subtitulo pt-1">
+              Lider del proyecto
+            </v-card-subtitle>                           
+            <v-card-text class="texto-centrado fuente-poppins">
+              Lorem ipsum dolor sit amet consectetur 
+              adipisicing elit. Vitae molestias dolorem eum, ab officia 
+              perferendis veniam, consectetur quas obcaecati facilis nisi 
+              fugiat modi, placeat unde quod ducimus optio! Quidem, debitis.
+            </v-card-text>                      
+          </v-card>          
+        </v-col>
+      </v-row>
+
+    </v-container>
+    <footerHome></footerHome>
   </div>
 </template>
 
 <script>
+import navbar from "../../components/navbar-home/navbar.vue";
+import footerHome from "../../components/footer-home/footer.vue";
+
 export default {
   name: "Equipo",
   data() {
     return {};
   },
+  components: {
+    navbar,
+    footerHome,
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap");
+
+.titulo-pagina{
+  font-family: "Poppins";
+  font: weight 700px;
+  font-size: 40px;
+  line-height: 40px;
+  color: #37474f;
+  text-align: center;
+}
+.titulo {
+  font-family: "Poppins";
+  font: weight 700px;
+  font-size: 36px;
+  line-height: 33px;
+  color: #37474f;
+  text-align: center;
+}
+.subtitulo{
+  font-family: "Poppins";
+  font-weight: 500px;
+  font-size: 20px;
+  color: white;
+  text-align: center;
+}
+.texto-centrado{
+  text-align: center;
+}
+.fuente-poppins{
+  font-family: "Poppins";
+}
+.logo {
+  display: block;
+  width: 250px;
+}
+.imagen-carta {
+  display: block;
+  width: 150px;
+  height: 150px;
+  opacity: 1;
+  background: transparent;
+}
+.linea{
+  width: 200px;
+  height: 0px;
+  border: 2px solid #495167;
+  opacity: 1;
+}
+.margen-abajo{
+  margin-bottom: 0px;
+}
+.sombra-avatar{
+  box-shadow: 0px 4px 6px #00000029;
+}
+
+
+</style>
